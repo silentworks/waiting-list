6eff2ed603632624b4b2762fe4166ad1ab81e90b
Andrew Smith <a.smith@silentworks.co.uk>
2022-02-14 14:33:48 +0000

    Update to use adapter auto for production

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-auto'
+import adapterNode from '@sveltejs/adapter-node'
 
 const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ env: { port: process.env.PORT } }),
+		adapter: dev
+			? adapterNode({ env: { port: process.env.PORT } })
+			: adapter({ env: { port: process.env.PORT } }),
 		vite: {
 			ssr: {
 				noExternal: dev ? [] : ['@supabase/supabase-js']
