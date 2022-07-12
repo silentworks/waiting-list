e38c3b26d775a6900a6209680888d47b9c082f2d
Andrew Smith <a.smith@silentworks.co.uk>
2022-07-12 15:17:03 +0000

    Update to latest sveltekit and auth-helpers

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
 import adapter from '@sveltejs/adapter-node'
 
-const dev = process.env.NODE_ENV === 'development'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		vite: {
-			ssr: {
-				noExternal: dev ? [] : ['@supabase/supabase-js']
-			}
-		}
 	}
 }
 
