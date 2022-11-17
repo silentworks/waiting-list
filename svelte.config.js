0886ccba380c5aff2c5460700f6462303e7fdf30
Andrew Smith <a.smith@silentworks.co.uk>
2022-11-17 11:38:35 +0000

    Change to use adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter()
 	}
 }
 
