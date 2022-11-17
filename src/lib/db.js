c1b989b33789bf7ecfd4ed65360ec79386538225
Andrew Smith <a.smith@silentworks.co.uk>
2022-11-17 01:55:26 +0000

    Update to use public env variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { env } from '$env/static/public'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
