9cff74514748da5d7d3bba3cec94bf5cfafd7bc4
Andrew Smith <a.smith@silentworks.co.uk>
2022-11-21 00:19:59 +0000

    Update where env variable comes from

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { PUBLIC_SUPABASE_URL } from '$env/static/public'
-import { env } from '$env/dynamic/private'
+import { SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private'
 
-const supabase = createClient(PUBLIC_SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY)
+const supabase = createClient(PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
 export default supabase
