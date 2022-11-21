bf9fff542d05d34c473a9196c290a7d47f5b9de2
Andrew Smith <a.smith@silentworks.co.uk>
2022-11-21 00:48:32 +0000

    Logging key

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private'
 
+console.log({ SUPABASE_SERVICE_ROLE_KEY })
 const supabase = createClient(PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
 export default supabase
