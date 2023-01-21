f8283376551d8746da69b3919361aa57ffa6e68d
Andrew Smith <a.smith@silentworks.co.uk>
2023-01-21 13:23:39 +0000

    Update to get port from connection string

--- ley.config.cjs
+++ ley.config.cjs
@@ -1,17 +1,16 @@
 require('dotenv').config()
 
-const options = {
-	port: 5432
-};
+const options = {};
 
 if (process.env.CONNECTION_STRING) {
 	const { parse } = require("pg-connection-string");
-	const  { host, database, user, password } = parse(process.env.CONNECTION_STRING);
+	const  { host, database, port, user, password } = parse(process.env.CONNECTION_STRING);
 
 	options.host = host;
 	options.database = database;
 	options.username = user;
 	options.password = password;
+	options.port = port;
 }
 
 module.exports = options;
